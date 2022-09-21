da2d52c6e50462222118c6526c2642ca7559f63b
joeldevel <christianjoel.developer@gmail.com>
2022-09-21 19:42:45 -0300

    adds react navigation basic example

--- semaine-1/src/App.js
+++ semaine-1/src/App.js
@@ -1,9 +1,19 @@
+import { Outlet, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div>Bonjour React!</div>
-
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet/>
+    </div>
   );
 }
-
-export default App;
